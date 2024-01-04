3b6f3b839d830fe803bbaac71acfe4d6fb5b2487
mgsanthosh <mgsanthosh553@gmail.com>
2024-01-04 19:12:07 +0530

    push commit

---
+++ currency-exchange-service/src/main/java/com/example/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,29 @@
+package com.example.currencyexchangeservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyExchangeController {
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    private CurrencyExchangeRepository currencyExchangeRepository;
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange retireveExchange(@PathVariable String from, @PathVariable String to)  throws Exception{
+        CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to);
+        if(currencyExchange == null) {
+            throw new RuntimeException("Unable to find Data");
+        }
+        String port = environment.getProperty("local.server.port");
+        currencyExchange.setEnvironment(port);
+        currencyExchangeRepository.findByFromAndTo(from, to);
+        return currencyExchange;
+    }
+}
